89dc97b04c8f9606c5c796968ed78536661f1539
Kevin Alavik <kevin@alavik.se>
2024-11-11 21:09:31 +0100

    fix: Fixed kernel version string to include correct commit hash and made commit script nicley format the commits

--- dev/commit.sh
+++ dev/commit.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+
 if [ -z "$1" ]; then
   echo "Error: Commit message is required as the first argument."
   exit 1
 fi
 
+git add .
 git commit -m "$1"
 
 commit_hash=$(git rev-parse --short HEAD)
 
+commit_dir="dist/${commit_hash}"
+mkdir -p "$commit_dir"
+
+echo "$1" > "${commit_dir}/DESCRIPTION"
+git format-patch -1 HEAD --stdout > "${commit_dir}/commit.patch"
+
 make
 
-scp Nekonix.iso root@uwubox:/srv/ftp/public/nekonix/dist/Nekonix-${commit_hash}.iso
+mv Nekonix.iso "${commit_dir}/Nekonix-${commit_hash}.iso"
 
-echo "Uploaded Nekonix.iso as Nekonix-${commit_hash}.iso"
+scp -r "$commit_dir" root@192.168.100.119:/srv/ftp/public/nekonix/dist/
+
+rm -rf "$commit_dir"
+
+echo "Uploaded ${commit_dir} as Nekonix-${commit_hash}.iso"
