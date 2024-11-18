3bf939a1f7ea463ce527d742a8f6c3994255480e
Kevin Alavik <kevin@alavik.se>
2024-11-18 17:23:29 +0100

    feat: virt_to_phys works.

--- kernel/src/mm/vmm.c
+++ kernel/src/mm/vmm.c
@@ -11,19 +11,19 @@ u64 virt_to_phys(u64 virt)
     u64 pml3_idx = (virt & (u64)0x1ff << 30) >> 30;
     u64 pml4_idx = (virt & (u64)0x1ff << 39) >> 39;
 
-    INFO("vmm", "PML1 index: %llu", pml1_idx);
-    INFO("vmm", "PML2 index: %llu", pml2_idx);
-    INFO("vmm", "PML3 index: %llu", pml3_idx);
-    INFO("vmm", "PML4 index: %llu", pml4_idx);
+    DEBUG("vmm", "(0x%.16llx) PML1 index: %llu", virt, pml1_idx);
+    DEBUG("vmm", "(0x%.16llx) PML2 index: %llu", virt, pml2_idx);
+    DEBUG("vmm", "(0x%.16llx) PML3 index: %llu", virt, pml3_idx);
+    DEBUG("vmm", "(0x%.16llx) PML4 index: %llu", virt, pml4_idx);
 
     u64 pml4_value = top_level[pml4_idx];
     if (pml4_value & 1)
     {
-        INFO("vmm", "PML3 is present!");
+        DEBUG("vmm", "(0x%.16llx) PML3 is present!", virt);
     }
     else
     {
-        INFO("vmm", "PML3 is not present.");
+        DEBUG("vmm", "(0x%.16llx) PML3 is not present.", virt);
     }
 
     u64 *pml3_table = (u64 *)HIGHER_HALF(pml4_value & 0x000FFFFFFFFFF000);
@@ -31,11 +31,11 @@ u64 virt_to_phys(u64 virt)
 
     if (pml2_value & 1)
     {
-        INFO("vmm", "PML2 is present!");
+        DEBUG("vmm", "(0x%.16llx) PML2 is present!", virt);
     }
     else
     {
-        INFO("vmm", "PML2 is not present.");
+        DEBUG("vmm", "(0x%.16llx) PML2 is not present.", virt);
     }
 
     u64 *pml2_table = (u64 *)HIGHER_HALF(pml2_value & 0x000FFFFFFFFFF000);
@@ -43,11 +43,11 @@ u64 virt_to_phys(u64 virt)
 
     if (pml1_value & 1)
     {
-        INFO("vmm", "PML1 is present!");
+        DEBUG("vmm", "(0x%.16llx) PML1 is present!", virt);
     }
     else
     {
-        INFO("vmm", "PML1 is not present.");
+        DEBUG("vmm", "(0x%.16llx) PML1 is not present.", virt);
     }
 
     u64 *pml1_table = (u64 *)HIGHER_HALF(pml1_value & 0x000FFFFFFFFFF000);
@@ -60,6 +60,10 @@ int vmm_init()
 {
     __asm__ volatile("mov %%cr3, %0" : "=r"(top_level) : : "memory");
     top_level = HIGHER_HALF(top_level);
-    virt_to_phys(0xffff800000000000);
+
+    u64 virt = (u64)HIGHER_HALF(pmm_request_page());
+    u64 phys = virt_to_phys(virt);
+    DEBUG("vmm", "Virtual address: 0x%.16llx, Physical address: 0x%.16llx", virt, phys);
+    pmm_free_page((u8 *)virt);
     return 0;
 }
